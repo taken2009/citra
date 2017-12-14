5e1b95f08e62154c6ddaa5a148923e4fc6eeae17
taken2009 <32885332+taken2009@users.noreply.github.com>
2017-12-14 14:08:23 +0900

    CheatsModule-2

--- src/citra_qt/main.h
+++ src/citra_qt/main.h
@@ -36,6 +36,7 @@ class QProgressBar;
 class RegistersWidget;
 class Updater;
 class WaitTreeWidget;
+class CheatDialog;
 
 // Multiplayer forward declarations
 class Lobby;
@@ -170,6 +171,7 @@ private slots:
     void OnAnnounceFailed(const Common::WebResult&);
     void OnSwapScreens();
     void OnConfigure();
+    void OnCheats();
     void OnToggleFilterBar();
     void OnDisplayTitleBars(bool);
     void ToggleFullscreen();
@@ -225,6 +227,7 @@ private:
 
     bool explicit_update_check = false;
     bool defer_update_prompt = false;
+    std::shared_ptr<class CheatDialog> cheatWindow;
 
     // Multiplayer windows
     Lobby* lobby = nullptr;
