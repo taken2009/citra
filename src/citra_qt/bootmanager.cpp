2ba727bf2936559fd2b5a22bd9d9b039e095058b
citrabot <citra@citra-emu.org>
2017-12-16 19:42:36 +0000

    Merge PR 3073

--- src/citra_qt/bootmanager.cpp
+++ src/citra_qt/bootmanager.cpp
@@ -111,12 +111,10 @@ GRenderWindow::GRenderWindow(QWidget* parent, EmuThread* emu_thread)
     setWindowTitle(QString::fromStdString(window_title));
 
     InputCommon::Init();
-    Network::Init();
 }
 
 GRenderWindow::~GRenderWindow() {
     InputCommon::Shutdown();
-    Network::Shutdown();
 }
 
 void GRenderWindow::moveContext() {
