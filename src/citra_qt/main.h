8a5f66293b8f5e57f0101635ad28fc853ac7b75b
taken2009 <32885332+taken2009@users.noreply.github.com>
2018-01-04 17:02:16 +0900

    [Qt] toolbar (add option "showToolbar" Shortcut "CTRL+D" )

--- src/citra_qt/main.h
+++ src/citra_qt/main.h
@@ -110,6 +110,7 @@ private:
 
     void ConnectWidgetEvents();
     void ConnectMenuEvents();
+    void ConnectToolbarEvents();
 
     bool LoadROM(const QString& filename);
     void BootGame(const QString& filename);
@@ -156,6 +157,7 @@ private slots:
     void OnPauseGame();
     void OnStopGame();
     void OnMenuReportCompatibility();
+    void OnResetGame();
     /// Called whenever a user selects a game in the game list widget.
     void OnGameListLoadFile(QString game_path);
     void OnGameListOpenSaveFolder(u64 program_id);
@@ -187,6 +189,7 @@ private slots:
     void OnCheckForUpdates();
     void OnOpenUpdater();
     void OnLanguageChanged(const QString& locale);
+    void Onshowtoolbar();
 
 private:
     void UpdateStatusBar();
@@ -194,6 +197,8 @@ private:
     void SetupUIStrings();
     void LoadTranslationT();
     void SetupUIStringsT();
+
+    QString current_game_path; // To reset the game
 
     Ui::MainWindow ui;
 
