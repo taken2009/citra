65bd2a16e3a36b6f9b1b13bca6ebbbcf0319ed1f
taken2009 <32885332+taken2009@users.noreply.github.com>
2018-01-09 13:26:25 +0900

    Add option to switch "Ignore format reinterpretation"

--- src/video_core/renderer_opengl/gl_rasterizer_cache.cpp
+++ src/video_core/renderer_opengl/gl_rasterizer_cache.cpp
@@ -1433,6 +1433,21 @@ void RasterizerCacheOpenGL::ValidateSurface(const Surface& surface, PAddr addr, 
             }
         }
 
+        if (Settings::values.use_bos) {
+          // HACK HACK HACK: Ignore format reinterpretation
+          // this is a placeholder for HW texture decoding/encoding
+          constexpr bool IGNORE_FORMAT_REINTERPRETING = true;
+          bool retry = false;
+          if (IGNORE_FORMAT_REINTERPRETING) {
+            for (const auto& pair : RangeFromInterval(dirty_regions, interval)) {
+              surface->invalid_regions.erase(pair.first & interval);
+               retry = true;
+             }
+           }
+          if (retry)
+              continue;
+            }
+
         // Load data from 3DS memory
         FlushRegion(params.addr, params.size);
         surface->LoadGLBuffer(params.addr, params.end);
