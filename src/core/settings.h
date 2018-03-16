31df1bfddafa8ec350d1ba154bedff0baaa66c29
taken2009 <32885332+taken2009@users.noreply.github.com>
2018-03-16 19:27:01 +0900

    Interim Add "frame limit " option to View, Shortcut "CTRL+Z"

--- src/core/settings.h
+++ src/core/settings.h
@@ -20,6 +20,13 @@ enum class LayoutOption {
     StereoscopicSingleScreen,
 };
 
+enum class FrameOption {
+    zero,
+    single,
+    doubble,
+    triple,
+};
+
 namespace NativeButton {
 enum Values {
     A,
@@ -121,6 +128,7 @@ struct Values {
     bool FMV_hack;
     u16 AddTicks;
 
+    FrameOption frame_option;
     LayoutOption layout_option;
     bool swap_screen;
     bool custom_layout;
