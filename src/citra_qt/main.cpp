5290d8c9b7681887ec0e766e92318f854503cbbb
taken2009 <32885332+taken2009@users.noreply.github.com>
2018-07-09 19:21:04 +0900

    Interim Add "frame limit " option to View, Shortcut "CTRL+A"

--- src/citra_qt/main.cpp
+++ src/citra_qt/main.cpp
@@ -247,6 +247,12 @@ void GMainWindow::InitializeWidgets() {
     actionGroup_ScreenLayouts->addAction(ui.action_Screen_Layout_Single_Screen);
     actionGroup_ScreenLayouts->addAction(ui.action_Screen_Layout_Large_Screen);
     actionGroup_ScreenLayouts->addAction(ui.action_Screen_Layout_Side_by_Side);
+
+    QActionGroup *actionGroup_frame = new QActionGroup(this);
+    actionGroup_frame->addAction(ui.action_frame_zero);
+    actionGroup_frame->addAction(ui.action_frame_single);
+    actionGroup_frame->addAction(ui.action_frame_doubble);
+    actionGroup_frame->addAction(ui.action_frame_triple);
 }
 
 void GMainWindow::InitializeDebugWidgets() {
@@ -346,6 +352,7 @@ void GMainWindow::InitializeHotkeys() {
     hotkey_registry.RegisterHotkey("Main Window", "Swap Screens", QKeySequence(tr("F9")));
     hotkey_registry.RegisterHotkey("Main Window", "Show Toolbar", QKeySequence("CTRL+Q"),
     hotkey_registry.RegisterHotkey("Main Window", "Toggle Screen Layout", QKeySequence(tr("F10")));
+    hotkey_registry.RegisterHotkey("Main Window", "Toggle Frame Limit", QKeySequence("CTRL+A"),
     hotkey_registry.RegisterHotkey("Main Window", "Fullscreen", QKeySequence::FullScreen);
     hotkey_registry.RegisterHotkey("Main Window", "Exit Fullscreen", QKeySequence(Qt::Key_Escape),
                                    Qt::ApplicationShortcut);
@@ -387,6 +394,8 @@ void GMainWindow::InitializeHotkeys() {
             &QShortcut::activated, ui.action_Show_Toolbar, &QAction::trigger);
     connect(hotkey_registry.GetHotkey("Main Window", "Toggle Screen Layout", render_window),
             &QShortcut::activated, this, &GMainWindow::ToggleScreenLayout);
+    connect(hotkey_registry.GetHotkey("Main Window", "Toggle Frame Limit", render_window),
+                        &QShortcut::activated, this, &GMainWindow::ToggleFramelimit);
     connect(hotkey_registry.GetHotkey("Main Window", "Fullscreen", render_window),
             &QShortcut::activated, ui.action_Fullscreen, &QAction::trigger);
     connect(hotkey_registry.GetHotkey("Main Window", "Fullscreen", render_window),
@@ -471,6 +480,8 @@ void GMainWindow::RestoreUIState() {
     statusBar()->setVisible(ui.action_Show_Status_Bar->isChecked());
 
     ui.action_Show_Toolbar->setChecked(UISettings::values.Show_Toolbar);
+
+    FramelimitUISettings();
 }
 
 void GMainWindow::ConnectWidgetEvents() {
@@ -523,6 +534,11 @@ void GMainWindow::ConnectMenuEvents() {
     connect(ui.action_Show_Filter_Bar, &QAction::triggered, this, &GMainWindow::OnToggleFilterBar);
     connect(ui.action_Show_Status_Bar, &QAction::triggered, statusBar(), &QStatusBar::setVisible);
     connect(ui.action_Show_Toolbar, &QAction::triggered, this, &GMainWindow::Onshowtoolbar);
+
+    connect(ui.action_frame_single, &QAction::triggered, this, &GMainWindow::Changeframelimit);
+    connect(ui.action_frame_zero, &QAction::triggered, this, &GMainWindow::Changeframelimit);
+    connect(ui.action_frame_doubble, &QAction::triggered, this, &GMainWindow::Changeframelimit);
+    connect(ui.action_frame_triple, &QAction::triggered, this, &GMainWindow::Changeframelimit);
 
     // Multiplayer
     connect(ui.action_View_Lobby, &QAction::triggered, multiplayer_state,
@@ -1371,6 +1387,7 @@ void GMainWindow::OnConfigure() {
         emit UpdateThemedIcons();
         SyncMenuUISettings();
         game_list->RefreshGameDirectory();
+        FramelimitUISettings();
         config->Save();
     }
 }
@@ -1381,6 +1398,55 @@ void GMainWindow::Onshowtoolbar(){
     }else{
         ui.toolbar->hide();
     }
+}
+
+void GMainWindow::Changeframelimit(){
+
+    Settings::FrameOption newlimit = Settings::FrameOption::zero;
+
+    if(ui.action_frame_zero->isChecked()){
+        newlimit = Settings::FrameOption::zero,Settings::values.frame_limit = 0,Settings::values.use_frame_limit = false;
+    }else if (ui.action_frame_single->isChecked()){
+        newlimit = Settings::FrameOption::single,Settings::values.frame_limit = 100,Settings::values.use_frame_limit = true;
+    }else if (ui.action_frame_doubble->isChecked()){
+        newlimit = Settings::FrameOption::doubble,Settings::values.frame_limit = 200,Settings::values.use_frame_limit = true;
+    }else if (ui.action_frame_triple->isChecked()){
+        newlimit = Settings::FrameOption::triple,Settings::values.frame_limit = 300,Settings::values.use_frame_limit = true;
+    }
+
+    Settings::values.frame_option = newlimit;
+    Settings::Apply();
+}
+
+void GMainWindow::ToggleFramelimit(){
+
+    Settings::FrameOption newlimit = Settings::FrameOption::zero;
+
+    switch (Settings::values.frame_option) {
+    case Settings::FrameOption::zero:
+        newlimit = Settings::FrameOption::single,Settings::values.frame_limit = 100,Settings::values.use_frame_limit = true;
+        break;
+    case Settings::FrameOption::single:
+        newlimit = Settings::FrameOption::doubble,Settings::values.frame_limit = 200,Settings::values.use_frame_limit = true;
+        break;
+    case Settings::FrameOption::doubble:
+        newlimit = Settings::FrameOption::triple,Settings::values.frame_limit = 300,Settings::values.use_frame_limit = true;
+        break;
+    case Settings::FrameOption::triple:
+        newlimit = Settings::FrameOption::zero,Settings::values.frame_limit = 0,Settings::values.use_frame_limit = false;
+        break;
+    }
+
+    Settings::values.frame_option = newlimit;
+    FramelimitUISettings();
+    Settings::Apply();
+}
+
+void GMainWindow::FramelimitUISettings(){
+    ui.action_frame_zero->setChecked(Settings::values.frame_option == Settings::FrameOption::zero);
+    ui.action_frame_single->setChecked(Settings::values.frame_option == Settings::FrameOption::single);
+    ui.action_frame_doubble->setChecked(Settings::values.frame_option == Settings::FrameOption::doubble);
+    ui.action_frame_triple->setChecked(Settings::values.frame_option == Settings::FrameOption::triple);
 }
 
 void GMainWindow::OnToggleFilterBar() {
@@ -1556,7 +1622,7 @@ void GMainWindow::UpdateStatusBar() {
 
     auto results = Core::System::GetInstance().GetAndResetPerfStats();
 
-    if (Settings::values.use_frame_limit) {
+    if (Settings::values.use_frame_limit || Settings::values.frame_limit > 0) {
         emu_speed_label->setText(tr("Speed: %1% / %2%")
                                      .arg(results.emulation_speed * 100.0, 0, 'f', 0)
                                      .arg(Settings::values.frame_limit));
