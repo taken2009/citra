ad843ac14de294a9efc0ddf4347ea574d7b249bb
taken2009 <32885332+taken2009@users.noreply.github.com>
2018-08-12 22:28:37 +0900

    [Qt] toolbar (add option "showToolbar" Shortcut "CTRL+Q" )

--- src/citra_qt/main.h
+++ src/citra_qt/main.h
@@ -105,6 +105,7 @@ private:
 
     void ConnectWidgetEvents();
     void ConnectMenuEvents();
+    void ConnectToolbarEvents();
 
     bool LoadROM(const QString& filename);
     void BootGame(const QString& filename);
@@ -152,6 +153,7 @@ private slots:
     void OnPauseGame();
     void OnStopGame();
     void OnMenuReportCompatibility();
+    void OnResetGame();
     /// Called whenever a user selects a game in the game list widget.
     void OnGameListLoadFile(QString game_path);
     void OnGameListOpenFolder(u64 program_id, GameListOpenTarget target);
@@ -189,6 +191,7 @@ private slots:
     void OnCheckForUpdates();
     void OnOpenUpdater();
     void OnLanguageChanged(const QString& locale);
+    void Onshowtoolbar();
 
 private:
     bool ValidateMovie(const QString& path, u64 program_id = 0);
@@ -197,6 +200,8 @@ private:
     void LoadTranslation();
     void SetupUIStrings();
     void RetranslateStatusBar();
+
+    QString current_game_path; // To reset the game
 
     Ui::MainWindow ui;
 
